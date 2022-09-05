6a59c38c7a345decf952dd4bbf6e931fbf895c6f
Bastian Karstaedt <2082580+bkarstaedt@users.noreply.github.com>
2022-09-05 12:53:15 +0000

    removed custom_domain folder

--- modules/webapps/static_site/custom_domain/custom_domain.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_static_site_custom_domain" "custom_domain" {
-  static_site_id  = var.static_site_id
-  domain_name     = var.domain_name
-  validation_type = var.validation_type
-}
--- modules/webapps/static_site/custom_domain/output.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = azurerm_static_site_custom_domain.custom_domain.id
-  description = "The ID of the Static Site Custom Domain."
-}
-
-output "validation_token" {
-  value       = azurerm_static_site_custom_domain.custom_domain.validation_token
-  description = "Token to be used with 'dns-txt-token' validation."
-}
